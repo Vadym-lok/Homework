228b752bd89c110b6e20f66de3f93a8db82bbfb9
Vadym <vadim.lukutinmail.ru@gmail.com>
2023-05-03 23:56:05 +0300

    add files

---
+++ work_7.py
@@ -0,0 +1,51 @@
+"""
+симулятор каси магазину
+код, наведений нижче, приймає від покупця наступну інформацію про закупівлю 2-х товарів
+- назва
+- кількість (ціле число)
+- ціна за одиницю
+на підставі отриманих даних формується чек
+всі ціни та вартість мають бути виведені в форматі з копійками, кількість - цілі числа
+програма розрахована на використання на території України
+"""
+import math
+import textwrap
+from datetime import datetime
+from decimal import Decimal
+# goods 1 section
+item_1_title = textwrap.shorten(input('Введіть назву першого товару: ').ljust(20, '.'), width=20, placeholder='...')
+item_1_quantity = int(input('Введіть бажаєму кількість першого товару: '))
+item_1_zina = float(input('Введіть ціну першого товару: '))
+
+# goods 2 section
+item_2_title = textwrap.shorten(input('Введіть назву другого товару: ').ljust(20, '.'), width=20, placeholder='...')
+item_2_quantity = int(input('Введіть бажаєму кількість другого товару: '))
+item_2_zina = float(input('Введіть ціну другого товару: '))
+
+item_1_total_cost = Decimal(item_1_quantity) * Decimal(item_1_zina)
+item_1_total_cost_right_format = item_1_total_cost.quantize(Decimal('1.0000'))
+
+item_2_total_cost = Decimal(item_2_quantity) * Decimal(item_2_zina)
+item_2_total_cost_right_format = item_2_total_cost.quantize(Decimal('1.0000'))
+sum_item_1_2 = item_1_quantity + item_2_quantity
+sum_total_cost = item_1_total_cost_right_format + item_2_total_cost_right_format
+
+printing_template = '{}\t\t\t\t\t{}\t\t\t\t{}\t\t\t{}'
+
+# printing receipt
+print('\n\n\n')
+print('фіскальний чек'.capitalize().center(80, '~'))
+print('магазин "все для дому"'.upper().center(80))
+print(f'Товар\t\t\t\t\t\t\t\t\tкількість\t\tціна\t\tвартість')
+print(printing_template.format(item_1_title, item_1_quantity, item_1_zina, round(item_1_total_cost_right_format, 2)))
+print(printing_template.format(item_2_title, item_2_quantity, item_2_zina, round(item_2_total_cost_right_format, 2)))
+print('~' * 80)
+print(printing_template.format(
+    'ВСЬОГО'.ljust(20),
+    sum_item_1_2,
+    'x',
+    round(sum_total_cost, 2)
+    )
+)
+print(datetime.now().strftime('%d-%m-%Y %H:%M:%S').rjust(80))
+print('\n\n')
