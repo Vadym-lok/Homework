d286d565388f0b839b69d9596b732e4ca19f3046
Vadym <vadim.lukutinmail.ru@gmail.com>
2023-05-20 23:24:06 +0300

    add files

---
+++ work_11.py
@@ -0,0 +1,36 @@
+from pprint import pprint
+student = {
+    'Іван Петров': {
+        'Пошта': 'Ivan@gmail.com',
+        'Вік': 14,
+        'Номер телефону': '+380987771221',
+        'Середній бал': 95.8
+    },
+    'Женя Курич': {
+        'Пошта': 'Geka@gmail.com',
+        'Вік': 16,
+        'Номер телефону': None,
+        'Середній бал': 64.5
+    },
+    'Маша Кера': {
+        'Пошта': 'Masha@gmail.com',
+        'Вік': 18,
+        'Номер телефону': '+380986671221',
+        'Середній бал': 80
+    },
+    'Грицай Влад': {
+        'Пошта': 'vlad.gad@gmail.com',
+        'Вік': 25,
+        'Номер телефону': '+38062716547',
+        'Середній бал': 91
+    }
+}
+average_score = 0
+number_of_students = len(student)
+for value in student:
+    if student[value]['Номер телефону'] == None:
+        student[value]['Номер телефону'] = '102'
+    elif student[value]['Середній бал'] > 90:
+        pprint(f" {value} Середній бал {student[value]['Середній бал']} ")
+    average_score += student[value]['Середній бал'] / number_of_students
+pprint(average_score)
