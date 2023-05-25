7af1a91cf901347b0fca2c839f5e5856e62018a8
Vadym <vadim.lukutinmail.ru@gmail.com>
2023-05-26 01:30:50 +0300

    Homework

--- work_13.py
+++ work_13.py
@@ -1,20 +1,23 @@
 import requests
 from pprint import pprint
-url = 'https://dummyjson.com/users'
+url = 'https://dummyjson.com/users?limit=100'
 response = requests.get(url)
 data = response.json()
+users_data = data['users']
 general_age = 0
 average_age = 0
 number_of_people = 0
-for value in data['users']:
-    products = value.get('hair')
+for value in users_data:
+    data['limit'] = 100
+    hair_color = value.get('hair')
     data_age = value.get('age')
     city_value = value.get('address')
-    if products['color'] == 'Brown':
+    city_data = city_value.get('city')
+    if hair_color['color'] == 'Brown':
         number_of_people += 1
         general_age += data_age
-        average_age = round(general_age / number_of_people, 1)
-    if city_value['city'] == 'Louisville':
-        pprint(city_value)
+    if city_data == 'Louisville':
+        pprint(value)
+average_age = round(general_age / number_of_people, 1)
 
 pprint(average_age)
